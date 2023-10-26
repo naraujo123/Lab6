b22174807abeacce438c447aac6323ebf1076902
NoahSwor <148926023+NoahSwor@users.noreply.github.com>
2023-10-25 23:11:19 -0400

    Add files via upload

---
+++ lab6main_encode_neil_NoahSworChanges.py
@@ -0,0 +1,51 @@
+ # Neil Araujo
+
+def decode():
+    print(f'The encoded password is {new_encoded_pw}, and the original password is {user_pw}.')
+
+def encode(user_pw):
+    key = {
+        3: 3,
+        4: 4,
+        5: 5,
+        6: 6,
+        7: 7,
+        8: 8,
+        9: 9,
+        10: 0,
+        11: 1,
+        12: 2
+    }
+    global encoded_str_pw
+    for i in range(8):
+        encoded_user_pw.append(key[(int(user_pw[i]) + 3)])
+        encoded_digit = str(encoded_user_pw[i])
+        encoded_str_pw = encoded_str_pw + encoded_digit
+    return encoded_str_pw
+
+encoded_user_pw = []
+encoded_str_pw = ''
+if __name__ == '__main__':
+
+    menu_run = True
+
+    while menu_run:
+        print('Menu\n-------------')
+        print('1. Encode\n2. Decode\n3. Quit')
+        print('')
+        user_selection = int(input('Please enter an option:'))
+        if user_selection == 1:
+            user_pw = (input('Please enter your password to encode: '))
+            print('Your password has been encoded and stored!')
+            print()
+            new_encoded_pw = encode(user_pw)
+        elif user_selection == 2:
+            decode()
+        elif user_selection == 3:
+            menu_run = False
+        else:
+            print('error')
+# Noah Swor's changes
+# added the decode function at the beginning of the file (since it needs to come before it is called in line 43)
+# changed multiple "If" statements to be "elif" and "else"
+# added an option if the user inputs a number besides 1, 2, or 3.
